d7aefa2f5dc5f6b51c0ac990ae00a544d5eeda47
Matt Steele <orphum@gmail.com>
2012-09-06 10:52:14 -0500

    Passing tests for statusList

--- specs/statusListSpec.js
+++ specs/statusListSpec.js
@@ -12,4 +12,18 @@ describe("Status List", function() {
   it('fetches records from the server', function() {
     expect(collection.fetch).toHaveBeenCalled();
   });
+
+  it('renders when collection is reset', function() {
+    collection.reset([{text: 'Reset text'}]);
+    var $li = $el.find('li');
+    expect($li.length).toBe(1);
+    expect($li.text()).toEqual('Reset text');
+  });
+
+  it('displays newly added elements', function() {
+    collection.add({text: 'Added text'});
+    var $li = $el.find('li');
+    expect($li.length).toBe(1);
+    expect($li.text()).toEqual('Added text');
+  });
 });
--- src/statusList.js
+++ src/statusList.js
@@ -1,5 +1,20 @@
 Monologue.View.StatusList = Backbone.View.extend({
   initialize: function() {
+    _.bindAll(this, 'render', 'add');
+    this.collection.on('reset', this.render);
+    this.collection.on('add', this.add);
     this.collection.fetch();
+  },
+
+  render: function() {
+    this.collection.each(_.bind(this.add, this));
+  },
+
+  add: function(model) {
+    this.$el.append(this.template(model));
+  },
+
+  template: function(model) {
+    return this.make('li', {'class':'status'}, model.get('text'));
   }
 });
