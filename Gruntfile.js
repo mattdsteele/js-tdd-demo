a7805d37cd076609f3991780231b8f2ca661328b
Matt Steele <orphum@gmail.com>
2013-03-11 22:08:50 -0500

    Making the app work again

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 /*global module:false*/
 module.exports = function(grunt) {
+  var jquery = [
+    'lib/jquery-1.8.1.js'
+    ];
+  var backbone = [
+    'lib/underscore.js',
+    'lib/backbone.js',
+    'lib/backbone.localStorage.js'
+      ];
+  var appCode = [
+    'src/app.js',
+    'src/status.js',
+    'src/statuses.js',
+    'src/postStatus.js',
+    'src/statusList.js'
+      ];
+  var prodCode = jquery.concat(backbone).concat(appCode);
 
   // Project configuration.
   grunt.initConfig({
@@ -34,13 +50,18 @@ module.exports = function(grunt) {
         src: 'src/**/*.js',
         options: {
           specs: 'specs/**/*Spec.js',
-          vendor: [
-            "lib/underscore.js",
-            "lib/jquery-1.8.1.js",
-            "lib/backbone.js",
-            "lib/backbone.localStorage.js"
-          ]
+          vendor: jquery.concat(backbone)
         }
+      }
+    },
+    concat: {
+      dist: {
+        src: prodCode,
+        dest: 'target/app.js'
+      },
+      cowboy: {
+        src: jquery.concat(['src/cowboy.js']),
+        dest: 'target/app-cowboy.js'
       }
     }
   });
@@ -48,8 +69,9 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task.
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['concat', 'jasmine']);
 
 };
