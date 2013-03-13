73cf71b5c3c8a35c6a6eee8b913d316fd8bd8b3b
Matt Steele <orphum@gmail.com>
2013-03-12 22:09:00 -0500

    Actually writing some services now

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/cowboy', routes.cowboy);
+app.post('/statuses', routes.statuses);
+app.get('/statuses', routes.statuses);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,7 @@ exports.index = function(req, res){
 exports.cowboy = function(req,res) {
   res.render('cowboy.html');
 };
+
+exports.statuses = function(req,res) {
+  res.send({ text: req.body.text });
+};
