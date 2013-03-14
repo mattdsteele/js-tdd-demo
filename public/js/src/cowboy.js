7c8ccce01e7cff3f8a1ec28c40055b6d7930cfb6
Matt Steele <orphum@gmail.com>
2013-03-13 22:35:16 -0500

    Through step 4

--- public/js/src/cowboy.js
+++ public/js/src/cowboy.js
@@ -1,25 +1,23 @@
+var Statuses = function() {};
+Statuses.prototype.add = function(options) {
+  $.ajax({
+    url: '/statuses',
+    type: 'POST',
+    dataType: 'json',
+    data: { text: options.text },
+    success: options.success
+  });
+};
+
 jQuery(function($) {
+  var statuses = new Statuses();
   $('form').on('submit', function() {
-    $.ajax({
-      url: '/statuses',
-      type: 'POST',
-      dataType: 'json',
-      data: {text: $(this).find('textarea').val()},
+    statuses.add({
+      text: $('textarea').val(),
       success: function(data) {
         $('#statuses').append('<li>' + data.text + '</li>');
       }
     });
     return false;
-  });
-
-  $.ajax({
-    url: '/statuses',
-    dataType: 'json',
-    success: function(data) {
-      var $statuses = $('#statuses');
-      for(var i = 0; data.length > i; i++) {
-        $statuses.append('<li>' + data[i].text + '</li>');
-      }
-    }
   });
 });
